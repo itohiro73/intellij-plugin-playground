1c845e8f35bc7f106fffc2db6150cc0673576011
Hiroshi Ito <itohiro73@gmail.com>
2015-09-12 11:56:54 +0900

    Import clean-up.

--- src/main/java/jp/itohiro/intellij/sample/domain/Qiita.java
+++ src/main/java/jp/itohiro/intellij/sample/domain/Qiita.java
@@ -1,19 +1,17 @@
 package jp.itohiro.intellij.sample.domain;
 
-import com.google.api.client.http.*;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson.JacksonFactory;
-import com.google.api.client.util.Key;
 import com.gs.collections.impl.list.fixed.ArrayAdapter;
 import jp.itohiro.intellij.sample.ui.ResultTable;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 
 public class Qiita implements Searchable {
     static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
--- src/main/java/jp/itohiro/intellij/sample/domain/StackOverflow.java
+++ src/main/java/jp/itohiro/intellij/sample/domain/StackOverflow.java
@@ -1,6 +1,5 @@
 package jp.itohiro.intellij.sample.domain;
 
-import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpTransport;
@@ -8,13 +7,11 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson.JacksonFactory;
-import com.google.api.client.util.Key;
 import com.gs.collections.impl.list.fixed.ArrayAdapter;
 import jp.itohiro.intellij.sample.ui.ResultTable;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.util.concurrent.Executors;
 
 public class StackOverflow implements Searchable {
     static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
